4982f3982b1a2ded1a84df6505b1e004c9903b22
JackF <jackf@jfletcher-l.corp.instructure.com>
2023-03-20 20:59:31 -0600

    Everything but orchards finished

---
+++ gitInfo.js
@@ -0,0 +1,89 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+var gitDefinition = ('Git is a software that allows indivduals and groups of people to track code projects. It is very efficient and also allows you to make notes of progress made so you can go back and check previous work.') 
+    console.log (gitDefinition)
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+var gitHubDefinition = ('Git hub is a web based platform that allows software developers to store and work in collaboration on projects using Git. A storehouse for work that can be accessed by multiple people. ')
+console.log(gitHubDefinition)
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+var gitInitDefinition = ('Git init creates a local repository that will later be linked to Github. It is a file that has multiple sub files within it.')
+console.log(gitInitDefinition)
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+var gitCloneDefinition = ('Git clone clones a repository and adds it into a new directory')
+console.log(gitCloneDefinition)
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+var gitStatusDefinition = ('Git status shows you what stage you are working on in your directory. It shows all things that have been staged and ehat is being tracked by git and what is not' ) 
+console.log(gitStatusDefinition)
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+var gitAddDefinition = ('Git add alows you to add files into your repository')
+console.log(gitAddDefinition)
+var gitAddCode = (' git add .')
+console.log(gitAddCode)
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+var gitCommitDefinition = ('Git commit creates a snapshot of your repository. Also allows you to leave comments on where in your repository you are')
+console.log(gitCommitDefinition)
+var gitCommitCode = (' git commit -m "insert comment"')
+console.log(gitCommitCode)
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+var gitPushDefinition = ('git Push is used to upload local repository content to a remote repository')
+console.log(gitPushDefinition)
---
+++ sdlc.js
@@ -0,0 +1,32 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+console.log(planning)
+var analysisOfRequirements = ('This is the second step, where the desied outcome from the software is studied and the team deicdes what the best steps to take to achieve the end goal are.')
+console.log(analysisOfRequirements)
+var design = ('This is the third step, where the team actually starts coding and creating the desired software')
+console.log(design)
+var testingAndIntegration = ('This is the fourth step, where the coding is put through tests to debug it and just check for errors in general')
+console.log(testingAndIntegration)
+var Implementation = ('this is the fifth step, where the program has now been desgined and tested and is now ready to be put into action. Esentially, this is th step where the software is put in place to do whatever it is designed to do')
+console.log(Implementation)
+var maintain = ('This is the final step, where now that the code is implemented and doing what its designed to do, software developers must keep the code up to date and fix any unexpected issues. Basically just keep the software up and running properly')
+console.log(maintain)
